2ccd504edfe5592dac4ca7075ccac9637bc54ebf
duy.kan1234 <duy.kan1234@gmail.com>
2026-01-30 06:41:03 +0000

    Add all project files correctly

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -8,7 +8,8 @@
     # pkgs.go
     # pkgs.python311
     # pkgs.python311Packages.pip
-    # pkgs.nodejs_20
+    pkgs.nodejs_20
+    pkgs.jest
     # pkgs.nodePackages.nodemon
   ];
   # Sets environment variables in the workspace
